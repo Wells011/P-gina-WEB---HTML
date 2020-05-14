847a2b2f98f4fbfa0a40c12212f8c1dc79402ab4
Wells011 <52050403+Wells011@users.noreply.github.com>
2020-05-14 09:45:04 -0300

    Add files via upload

---
+++ principal.php
@@ -0,0 +1,42 @@
+<div class="container-fluid bg-3 text-center">    
+  <h3>Alguns pensamentos...</h3><br>
+  <div class="row">
+    <div class="col-sm-3">
+      <a href="poema1.php">"Buraco Negro"</a>
+      <img src="https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2018/08/buraco-negro-nasa.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <a href="poema2.php">"Invisível"</a>
+      <img src="https://pm1.narvii.com/6975/a446b7a85eaba2a10fbbe5085c71c79366cbbefcr1-1080-591v2_hq.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <a href="poema3.php">"Fagulhas"</a>
+      <img src="https://i.ytimg.com/vi/x4mMhhN1VY4/maxresdefault.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3">
+      <a href="poema4.php">"Marionetes"</a>
+      <img src="https://pantokrator.org.br/po/wp-content/uploads/2013/04/marionetes.jpg" class="img-responsive" style="width:100%" alt="Image">
+    </div>
+  </div>
+</div><br>
+
+<div class="container-fluid bg-3 text-center">    
+  <div class="row">
+    <div class="col-sm-3">
+      <a href="poema5.php">"Desculpa, amigo"</a>
+      <img src="https://1.bp.blogspot.com/-2QZFonOKcVQ/VxqONbfm0PI/AAAAAAAAlQ8/KrEssjVmRko6eZ7qAu1YU8Gky5xqtvZMQCLcB/w1200-h630-p-k-no-nu/luvvvv.jpg" class="img-responsive" style ="width:100%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <a href="poema6.php">"Máscaras"</a>
+      <img src="https://somosmalasana.eldiario.es/wp-content/uploads/2019/01/triste.jpg" class="img-responsive" style="width:94%" alt="Image">
+    </div>
+    <div class="col-sm-3"> 
+      <a href="poema7.php">"Alma"</a>
+      <img src="https://osegredo.com.br/wp-content/uploads/2017/09/Como-ter-uma-conex%C3%A3o-alma-profunda-com-cada-signo-do-zod%C3%ADaco-site-830x450.jpg" class="img-responsive" style="width:100%" alt="image">
+    </div>
+    <div class="col-sm-3">
+      <a href="poema8.php">"Solidão"</a>
+      <img src="https://cdn.leouve.com.br/2019/08/02101538/xsolidao.jpg.pagespeed.ic_.Xcw-5cz2Dx.jpg" class="img-responsive" style="width:96%" alt="Image">
+    </div>
+  </div>
+</div><br><br>
